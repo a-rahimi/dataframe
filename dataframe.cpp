93275da913289301d0f9862781c7343a33001384
Ali Rahimi <alrhim@amazon.com>
2023-11-23 22:10:29 -0800

    implement Grouping operation.
    you can now reduce the rows of a dataframe that have a common tag.
    
    I changed the syntax for Join from Join(df1,df2).op() to Join::op(df1, df2).
    The previous syntax is tricky in C++ because the object Join(df1, df2) has to
    hold a reference its argument until the method .op() is called. This became a problem
    when I tried to implement groupby by calling Join(df, index_of(df)).op() because
    index_of(df) is a temporary object that goes out of scope when .op() is called. I could
    have avoided this by instead calling
    
      auto i = index_of(df)
      return Join(df, i).op()
    
    but the fact that these two version seems so similar but one crashes implies a
    brittle API.
    
    So instead grouping just calls Join::op(df, index_of(df)), and there is no
    concern with index_of returning a temporary object.

--- dataframe.cpp
+++ dataframe.cpp
@@ -58,7 +58,7 @@ struct DataFrame
     template <typename ValueOther>
     DataFrame<Tag, Value> operator[](const DataFrame<Tag, ValueOther> &index)
     {
-        return merge(*this, index, Value(), IndexReduceOp<Tag, Value>());
+        return merge(*this, index, IndexReduceOp<Tag, Value>(), IndexReduceOp<Tag, Value>());
     }
 
     // By default, nothing gets added to the dataframe.
@@ -81,10 +81,7 @@ std::ostream &
 operator<<(std::ostream &s, const DataFrame<Tag, Value> &df)
 {
     for (size_t i = 0; i < df.size(); ++i)
-    {
-        auto r = df[i];
-        s << r.first << '\t' << r.second << std::endl;
-    }
+        s << df.tags[i] << '\t' << df.values[i] << std::endl;
     return s;
 }
 
@@ -108,7 +105,8 @@ struct DataFrame<RangeTag, Value>
     size_t size() const { return values.size(); }
 };
 
-// A dataframe with no values. Just tags.
+// A dataframe with no values. Just tags. These are useful for indexing
+// operations, for example.
 template <typename Tag>
 struct DataFrame<Tag, NoValue>
 {
@@ -121,11 +119,22 @@ struct DataFrame<Tag, NoValue>
     size_t size() const { return tags.size(); }
 };
 
-template <typename Op, typename Tag, typename Value1, typename Value2, typename Tout>
-static auto
-merge(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, Tout init, Op merge_elements)
+template <typename Tag>
+std::ostream &
+operator<<(std::ostream &s, const DataFrame<Tag, NoValue> &df)
 {
-    auto df_out = DataFrame(Tag(), merge_elements(Tag(), Tag(), Value1(), Value2()).second);
+    s << '[';
+    for (size_t i = 0; i < df.size(); ++i)
+        s << df.tags[i] << ", ";
+    s << ']';
+    return s;
+}
+
+template <typename Merge12Op, typename AccumulateOp, typename Tag, typename Value1, typename Value2>
+static auto
+merge(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, Merge12Op merge_12, AccumulateOp accumulate)
+{
+    auto df_out = DataFrame(Tag(), merge_12(Tag(), Tag(), Value1(), Value2()).second);
 
     size_t i1 = 0, i2 = 0;
 
@@ -134,14 +143,14 @@ merge(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, Tout
         // Process tags in df1 that are smaller than the current tag in df2.
         while ((df1.tags[i1] < df2.tags[i2]) && (i1 < df1.size()))
         {
-            auto acc = merge_elements(df1.tags[i1], NoTag(), df1.values[i1], init);
+            auto acc = merge_12(df1.tags[i1], NoTag(), df1.values[i1], NoValue());
             Tag tag1 = df1.tags[i1];
 
             i1++;
 
             // Combine the run of identical tags from df1.
             for (; df1.tags[i1] == tag1; ++i1)
-                acc = merge_elements(df1.tags[i1], NoTag(), df1.values[i1], acc.second);
+                acc = accumulate(df1.tags[i1], NoTag(), df1.values[i1], acc.second);
 
             df_out.push_back(acc);
         }
@@ -149,13 +158,13 @@ merge(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, Tout
         // Process tags in df1 that match the current tag in df2.
         if (df1.tags[i1] == df2.tags[i2])
         {
-            auto acc = merge_elements(df1.tags[i1], df2.tags[i2], df1.values[i1], df2.values[i2]);
+            auto acc = merge_12(df1.tags[i1], df2.tags[i2], df1.values[i1], df2.values[i2]);
 
             i1++;
 
             // Combine the run of identical tags from df1.
             for (; (df1.tags[i1] == df2.tags[i2]) && (i1 < df1.size()); ++i1)
-                acc = merge_elements(df1.tags[i1], df2.tags[i2], df1.values[i1], acc.second);
+                acc = accumulate(df1.tags[i1], df2.tags[i2], df1.values[i1], acc.second);
 
             // Push the result for all repeated tags in df2.
             for (Tag tag2 = df2.tags[i2]; (i2 < df2.size()) && (df2.tags[i2] == tag2); ++i2)
@@ -171,13 +180,13 @@ merge(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, Tout
     // Process remaining tags in df1.
     while (i1 < df1.size())
     {
-        auto acc = merge_elements(df1.tags[i1], NoTag(), df1.values[i1], init);
+        auto acc = merge_12(df1.tags[i1], NoTag(), df1.values[i1], NoValue());
 
         i1++;
 
         // Combine the run of identical tags from df1.
         for (Tag tag1 = df1.tags[i1]; (tag1 == df1.tags[i1]) && (i1 < df1.size()); ++i1)
-            acc = merge_elements(df1.tags[i1], NoTag(), df1.values[i1], acc.second);
+            acc = accumulate(df1.tags[i1], NoTag(), df1.values[i1], acc.second);
 
         df_out.push_back(acc);
     }
@@ -185,12 +194,25 @@ merge(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, Tout
     return df_out;
 }
 
-template <typename ReduceOp, typename Tag, typename Value1, typename Value2>
-struct JoinReductionOpAdaptor
+template <typename Tag, typename Value1, typename Value2>
+struct JoinSumOp
 {
-    ReduceOp op;
-    JoinReductionOpAdaptor(ReduceOp _op, Tag, Value1, Value2) : op(_op) {}
+    // By default, joining generates a NoTag
+    template <typename T1, typename T2, typename V1, typename V2>
+    auto operator()(T1 tag1, T2 tag2, V1 v1, V2 v2)
+    {
+        return std::pair(NoTag(), v1);
+    }
+    // Tags match and v1 and v2 have the expected type.
+    auto operator()(Tag tag1, Tag tag2, Value1 v1, Value2 v2)
+    {
+        return std::pair(tag1, v1 + v2);
+    }
+};
 
+template <typename Tag, typename Value1>
+struct JoinSumOp<Tag, Value1, NoValue>
+{
     // By default, joining generates a NoTag
     template <typename T1, typename T2, typename V1, typename V2>
     auto operator()(T1 tag1, T2 tag2, V1 v1, V2 v2)
@@ -198,9 +220,10 @@ struct JoinReductionOpAdaptor
         return std::pair(NoTag(), v1);
     }
 
-    auto operator()(Tag tag1, Tag tag2, Value1 v1, Value2 v2)
+    // Tags match but v2 is NoValue().
+    auto operator()(Tag tag1, Tag tag2, Value1 v1, NoValue v2)
     {
-        return std::pair(tag1, op(v1, v2));
+        return std::pair(tag1, v1);
     }
 };
 
@@ -229,33 +252,82 @@ struct JoinPairOp
     }
 };
 
-template <typename Tag, typename Value1, typename Value2>
 struct Join
 {
-    const DataFrame<Tag, Value1> &df1;
-    const DataFrame<Tag, Value2> &df2;
-
-    Join(const DataFrame<Tag, Value1> &_df1, const DataFrame<Tag, Value2> &_df2) : df1(_df1), df2(_df2) {}
-
-    auto sum()
+    template <typename Tag, typename Value1, typename Value2>
+    static auto sum(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2)
     {
-        return merge(df1, df2, Value1(0), JoinReductionOpAdaptor([](Value1 v1, Value2 v2)
-                                                                 { return v1 + v2; },
-                                                                 Tag(), Value1(), Value2()));
+        return merge(df1, df2, JoinSumOp<Tag, Value1, Value2>(), JoinSumOp<Tag, Value1, Value1>());
     }
 
-    auto pair()
+    template <typename Tag, typename Value1, typename Value2>
+    static auto pair(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2)
     {
-        return merge(df1, df2, std::pair<Value1, Value2>(), JoinPairOp<Tag, Value1, Value2>());
+        return merge(df1, df2, JoinPairOp<Tag, Value1, Value2>(), JoinPairOp<Tag, Value1, Value2>());
     }
 };
+
+template <typename Tag, typename Value>
+auto first_tags(const DataFrame<Tag, Value> &df)
+{
+    auto df_out = DataFrame<Tag, NoValue>();
+
+    if (!df.size())
+        return df_out;
+
+    df_out.tags.push_back(df.tags[0]);
+    for (size_t i = 1; i < df.size(); ++i)
+    {
+        if (df.tags[i] != df_out.tags.back())
+            df_out.tags.push_back(df.tags[i]);
+    }
+
+    return df_out;
+}
+
+struct Group
+{
+    template <typename Tag, typename Value>
+    static auto sum(const DataFrame<Tag, Value> &df)
+    {
+        return Join::sum(df, first_tags(df));
+    }
+};
+
+void test_first_tags()
+{
+    auto df = DataFrame<int, float>{{1, 2, 2, 3}, {10., 20., 100., 30.}};
+    auto dft = first_tags(df);
+
+    // TODO: test for this.
+    // assert(dft.values[100] == NoValue());
+    assert(dft.size() == 3);
+
+    assert(dft.tags[0] == 1);
+    assert(dft.tags[1] == 2);
+    assert(dft.tags[2] == 3);
+}
+
+void test_group_sum()
+{
+    auto df = DataFrame<int, float>{{1, 2, 2, 3}, {10., 20., 100., 30.}};
+
+    auto g = Group::sum(df);
+
+    assert(g.tags[0] == 1);
+    assert(g.values[0] == 10.f);
+    assert(g.tags[1] == 2);
+    assert(g.values[1] == 120.f);
+    assert(g.tags[2] == 3);
+    assert(g.values[2] == 30.);
+}
 
 void test_join_simple()
 {
     auto df1 = DataFrame<int, float>{{1, 2, 3}, {10., 20., 30.}};
     auto df2 = DataFrame<int, float>{{1, 2, 3}, {-11., -22., -33.}};
 
-    auto g = Join(df1, df2).sum();
+    auto g = Join::sum(df1, df2);
 
     assert(g.size() == 3);
 
@@ -272,7 +344,7 @@ void test_join_duplicates_left()
     auto df1 = DataFrame<int, float>{{1, 2, 2, 3}, {10., 20., 100., 30.}};
     auto df2 = DataFrame<int, float>{{1, 2, 3}, {-11., -22., -33.}};
 
-    auto g = Join(df1, df2).sum();
+    auto g = Join::sum(df1, df2);
 
     assert(g.size() == 3);
 
@@ -289,7 +361,7 @@ void test_join_binary()
     auto df1 = DataFrame<int, float>{{0, 0, 1}, {10., 20., 30.}};
     auto df2 = DataFrame<int, float>{{0, 1, 1}, {-11., -22., -33.}};
 
-    auto g = Join(df1, df2).sum();
+    auto g = Join::sum(df1, df2);
 
     assert(g.size() == 3);
     assert(g.tags[0] == 0);
@@ -305,7 +377,7 @@ void test_join_simple_pair()
     auto df1 = DataFrame<int, float>{{1, 2, 3}, {10., 20., 30.}};
     auto df2 = DataFrame<int, float>{{1, 2, 3}, {-11., -22., -33.}};
 
-    auto g = Join(df1, df2).pair();
+    auto g = Join::pair(df1, df2);
 
     assert(g.size() == 3);
     assert(g.tags[0] == 1);
@@ -350,6 +422,8 @@ void test_index_no_values()
 
 int main()
 {
+    test_first_tags();
+    test_group_sum();
     test_index_no_values();
     test_join_simple_pair();
     test_join_duplicates_left();
