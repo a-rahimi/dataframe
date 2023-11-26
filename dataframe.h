0a2796ac40f777aaf95222eff01bcbd5cda65f6e
Ali Rahimi <alrhim@amazon.com>
2023-11-26 08:23:29 -0800

    Configure automatic formatting

--- dataframe.h
+++ dataframe.h
@@ -4,66 +4,52 @@
 #include <utility>
 #include <vector>
 
-struct NoTag
-{
-};
+struct NoTag {};
 
-struct NoValue
-{
-};
+struct NoValue {};
 
 template <typename Tag, typename Value>
-struct IndexReduceOp
-{
+struct IndexReduceOp {
     // Indexing fails if the tag or values don't have the right type.
     template <typename T, typename TO, typename V, typename VO>
-    auto operator()(T tag, TO, V v, VO)
-    {
+    auto operator()(T tag, TO, V v, VO) {
         return std::pair(NoTag(), v);
     }
 
     // A successful indexing reduction in df1[df2] takes the value of df1, and
     // ignores the value of df2.
     template <typename ValueOther>
-    auto operator()(Tag tag, Tag, Value v, ValueOther)
-    {
+    auto operator()(Tag tag, Tag, Value v, ValueOther) {
         return std::pair(tag, v);
     }
 };
 
 template <typename Tag, typename Value>
-struct DataFrame
-{
+struct DataFrame {
     std::vector<Tag> tags;
     std::vector<Value> values;
 
     size_t size() const { return tags.size(); }
 
     template <typename ValueOther>
-    DataFrame<Tag, Value> operator[](const DataFrame<Tag, ValueOther> &index)
-    {
+    DataFrame<Tag, Value> operator[](const DataFrame<Tag, ValueOther> &index) {
         return merge(*this, index, IndexReduceOp<Tag, Value>(), IndexReduceOp<Tag, Value>());
     }
 
     // By default, nothing gets added to the dataframe.
     template <typename T, typename V>
-    void push_back(const std::pair<T, V> &)
-    {
-    }
+    void push_back(const std::pair<T, V> &) {}
 
     // Only push back pairs with the expected tag and type.
     template <>
-    void push_back(const std::pair<Tag, Value> &p)
-    {
+    void push_back(const std::pair<Tag, Value> &p) {
         tags.push_back(p.first);
         values.push_back(p.second);
     }
 };
 
 template <typename Tag, typename Value>
-std::ostream &
-operator<<(std::ostream &s, const DataFrame<Tag, Value> &df)
-{
+std::ostream &operator<<(std::ostream &s, const DataFrame<Tag, Value> &df) {
     for (size_t i = 0; i < df.size(); ++i)
         s << df.tags[i] << '\t' << df.values[i] << std::endl;
     return s;
@@ -74,8 +60,7 @@ struct RangeTag;
 // A std::vector for sequential tags. Instead of storing the tag values,
 // computes them as needed.
 template <>
-struct std::vector<RangeTag>
-{
+struct std::vector<RangeTag> {
     size_t start, end, step = 1;
 
     size_t size() const { return (end - start) / step; }
@@ -88,16 +73,14 @@ These require special handling because the indexing operation returns a
 different type of dataframe.
 */
 template <typename Value>
-struct DataFrame<RangeTag, Value>
-{
+struct DataFrame<RangeTag, Value> {
     std::vector<RangeTag> tags;
     std::vector<Value> values;
 
     size_t size() const { return values.size(); }
 
     template <typename ValueOther>
-    DataFrame<size_t, Value> operator[](const DataFrame<size_t, ValueOther> &index)
-    {
+    DataFrame<size_t, Value> operator[](const DataFrame<size_t, ValueOther> &index) {
         return merge(*this, index, IndexReduceOp<size_t, Value>(), IndexReduceOp<size_t, Value>());
     }
 };
@@ -105,15 +88,12 @@ struct DataFrame<RangeTag, Value>
 // DataFrames of type NoValue use a special version of std::vector that takes up
 // no space.
 template <>
-struct std::vector<NoValue>
-{
+struct std::vector<NoValue> {
     NoValue operator[](size_t i) const { return NoValue(); }
 };
 
 template <typename Tag>
-std::ostream &
-operator<<(std::ostream &s, const DataFrame<Tag, NoValue> &df)
-{
+std::ostream &operator<<(std::ostream &s, const DataFrame<Tag, NoValue> &df) {
     s << '[';
     for (size_t i = 0; i < df.size(); ++i)
         s << df.tags[i] << ", ";
@@ -122,19 +102,17 @@ operator<<(std::ostream &s, const DataFrame<Tag, NoValue> &df)
 }
 
 template <typename Merge12Op, typename AccumulateOp, typename Tag1, typename Value1, typename Tag2, typename Value2>
-auto merge(const DataFrame<Tag1, Value1> &df1, const DataFrame<Tag2, Value2> &df2, Merge12Op merge_12, AccumulateOp accumulate)
-{
+auto merge(const DataFrame<Tag1, Value1> &df1, const DataFrame<Tag2, Value2> &df2, Merge12Op merge_12,
+           AccumulateOp accumulate) {
     using TagOut = decltype(merge_12(df1.tags[0], df2.tags[0], df1.values[0], df2.values[0]).first);
     using ValueOut = decltype(merge_12(df1.tags[0], df2.tags[0], df1.values[0], df2.values[0]).second);
     DataFrame<TagOut, ValueOut> df_out;
 
     size_t i1 = 0, i2 = 0;
 
-    while (i2 < df2.size())
-    {
+    while (i2 < df2.size()) {
         // Process tags in df1 that are smaller than the current tag in df2.
-        while ((df1.tags[i1] < df2.tags[i2]) && (i1 < df1.size()))
-        {
+        while ((df1.tags[i1] < df2.tags[i2]) && (i1 < df1.size())) {
             auto acc = merge_12(df1.tags[i1], NoTag(), df1.values[i1], NoValue());
             auto tag1 = df1.tags[i1];
 
@@ -148,8 +126,7 @@ auto merge(const DataFrame<Tag1, Value1> &df1, const DataFrame<Tag2, Value2> &df
         }
 
         // Process tags in df1 that match the current tag in df2.
-        if (df1.tags[i1] == df2.tags[i2])
-        {
+        if (df1.tags[i1] == df2.tags[i2]) {
             auto acc = merge_12(df1.tags[i1], df2.tags[i2], df1.values[i1], df2.values[i2]);
 
             i1++;
@@ -161,17 +138,14 @@ auto merge(const DataFrame<Tag1, Value1> &df1, const DataFrame<Tag2, Value2> &df
             // Push the result for all repeated tags in df2.
             for (auto tag2 = df2.tags[i2]; (i2 < df2.size()) && (df2.tags[i2] == tag2); ++i2)
                 df_out.push_back(acc);
-        }
-        else
-        {
+        } else {
             // df1 had no tags that matched df2.
             ++i2;
         }
     }
 
     // Process remaining tags in df1.
-    while (i1 < df1.size())
-    {
+    while (i1 < df1.size()) {
         auto acc = merge_12(df1.tags[i1], NoTag(), df1.values[i1], NoValue());
 
         i1++;
@@ -187,8 +161,7 @@ auto merge(const DataFrame<Tag1, Value1> &df1, const DataFrame<Tag2, Value2> &df
 }
 
 template <typename ReductionOp, typename Tag, typename Value1, typename Value2>
-struct ReductionAdaptor
-{
+struct ReductionAdaptor {
     ReductionOp op;
 
     using ValueAccumulation = decltype(op(Value1(), Value2()));
@@ -197,30 +170,26 @@ struct ReductionAdaptor
 
     // By default, joining generates a NoTag
     template <typename T1, typename T2, typename V1, typename V2>
-    auto operator()(T1, T2, V1 v1, V2)
-    {
+    auto operator()(T1, T2, V1 v1, V2) {
         return std::pair(NoTag(), v1);
     }
 
     // Tags match and v1 and v2 have the expected type.
     template <>
-    auto operator()(Tag tag1, Tag, Value1 v1, Value2 v2)
-    {
+    auto operator()(Tag tag1, Tag, Value1 v1, Value2 v2) {
         return std::pair(tag1, op(v1, v2));
     }
 
     // Tags match and v1 has the expected type and v2 is the accumulation type
     template <>
-    auto operator()(Tag tag1, Tag, Value1 v1, ValueAccumulation va)
-    {
+    auto operator()(Tag tag1, Tag, Value1 v1, ValueAccumulation va) {
         return std::pair(tag1, op(v1, va));
     }
 };
 
 // Specialize the reduction adaptor to when DataFrame2 is a NoValue dataframe.
 template <typename ReductionOp, typename Tag, typename Value1>
-struct ReductionAdaptor<ReductionOp, Tag, Value1, NoValue>
-{
+struct ReductionAdaptor<ReductionOp, Tag, Value1, NoValue> {
     ReductionOp op;
 
     using ValueAccumulation = decltype(op(Value1(), Value1()));
@@ -229,28 +198,22 @@ struct ReductionAdaptor<ReductionOp, Tag, Value1, NoValue>
 
     // By default, joining generates a NoTag
     template <typename T1, typename T2, typename V1, typename V2>
-    auto operator()(T1, T2, V1 v1, V2)
-    {
+    auto operator()(T1, T2, V1 v1, V2) {
         return std::pair(NoTag(), v1);
     }
 
     // Tags match and v1 has the expected type and v2 is the accumulation type
     template <>
-    auto operator()(Tag tag1, Tag, Value1 v1, ValueAccumulation va)
-    {
+    auto operator()(Tag tag1, Tag, Value1 v1, ValueAccumulation va) {
         return std::pair(tag1, op(v1, va));
     }
 
     // Tags match but v2 is NoValue().
-    auto operator()(Tag tag1, Tag, Value1 v1, NoValue)
-    {
-        return std::pair(tag1, v1);
-    }
+    auto operator()(Tag tag1, Tag, Value1 v1, NoValue) { return std::pair(tag1, v1); }
 };
 
 template <typename CollateOp, typename Tag, typename Value1, typename Value2>
-struct CollateAdaptor
-{
+struct CollateAdaptor {
     CollateOp op;
 
     using ValueAccumulation = decltype(op(Value1(), Value2()));
@@ -259,51 +222,38 @@ struct CollateAdaptor
 
     // The default behavior when combining rows is to not merge.
     template <typename T1, typename T2, typename V1, typename V2>
-    auto operator()(T1, T2, V1, V2 v2)
-    {
+    auto operator()(T1, T2, V1, V2 v2) {
         return std::pair(NoTag(), v2);
     }
 
     // Combining a Value1 and a Value2 when tags match.
     template <>
-    auto operator()(Tag tag1, Tag, Value1 v1, Value2 v2)
-    {
+    auto operator()(Tag tag1, Tag, Value1 v1, Value2 v2) {
         return std::pair(tag1, op(v1, v2));
     }
 
     // Accumulating results does nothing.
     template <>
-    auto operator()(Tag tag1, Tag, Value1 v1, ValueAccumulation acc)
-    {
+    auto operator()(Tag tag1, Tag, Value1 v1, ValueAccumulation acc) {
         return std::pair(tag1, acc);
     }
 };
 
-struct Join
-{
+struct Join {
     template <typename Tag, typename Value1, typename Value2>
-    static auto sum(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2)
-    {
-        auto collate_op = ReductionAdaptor(
-            [](Value1 v1, Value2 v2)
-            { return v1 + v2; },
-            Tag(), Value1(), Value2());
+    static auto sum(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2) {
+        auto collate_op = ReductionAdaptor([](Value1 v1, Value2 v2) { return v1 + v2; }, Tag(), Value1(), Value2());
         return merge(df1, df2, collate_op, collate_op);
     }
 
     template <typename Tag, typename Value1>
-    static auto sum(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, NoValue> &df2)
-    {
-        auto collate_op = ReductionAdaptor(
-            [](Value1 v1, Value1 va)
-            { return v1 + va; },
-            Tag(), Value1(), NoValue());
+    static auto sum(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, NoValue> &df2) {
+        auto collate_op = ReductionAdaptor([](Value1 v1, Value1 va) { return v1 + va; }, Tag(), Value1(), NoValue());
         return merge(df1, df2, collate_op, collate_op);
     }
 
     template <typename Tag, typename Value1, typename Value2, typename CollateOp>
-    static auto collate(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, CollateOp op)
-    {
+    static auto collate(const DataFrame<Tag, Value1> &df1, const DataFrame<Tag, Value2> &df2, CollateOp op) {
         auto adapted_op = CollateAdaptor(op, Tag(), Value1(), Value2());
         return merge(df1, df2, adapted_op, adapted_op);
     }
@@ -313,8 +263,7 @@ struct Join
  * has NoValue as its value type).
  */
 template <typename Tag, typename Value>
-DataFrame<Tag, NoValue> uniquify_tags(const DataFrame<Tag, Value> &df)
-{
+DataFrame<Tag, NoValue> uniquify_tags(const DataFrame<Tag, Value> &df) {
     // TODO: can i re-write this using merge() or collate()?
     DataFrame<Tag, NoValue> df_out;
 
@@ -329,11 +278,9 @@ DataFrame<Tag, NoValue> uniquify_tags(const DataFrame<Tag, Value> &df)
     return df_out;
 }
 
-struct Group
-{
+struct Group {
     template <typename Tag, typename Value>
-    static auto sum(const DataFrame<Tag, Value> &df)
-    {
+    static auto sum(const DataFrame<Tag, Value> &df) {
         return Join::sum(df, uniquify_tags(df));
     }
 };
--- test_dataframe.cpp
+++ test_dataframe.cpp
@@ -3,92 +3,95 @@ Compile this demo with
 
    clang++ -Wall -std=c++2b  test_dataframe.cpp  -lgtest_main -lgtest
 */
-#include <string>
-
 #include <gtest/gtest.h>
+
+#include <string>
 
 #include "dataframe.h"
 
-TEST(RangeTags, Simple)
-{
-    auto df = DataFrame<RangeTag, int>{.tags = {0, 5}, .values = {-1, -2, -3, -4, -5}};
-    auto i = DataFrame<size_t, NoValue>{{2, 3}};
+TEST(RangeTags, Simple) {
+    auto df = DataFrame<RangeTag, int>{
+        .tags = {0, 5},
+          .values = {-1, -2, -3, -4, -5}
+    };
+    auto i = DataFrame<size_t, NoValue>{
+        {2, 3}
+    };
     auto c = df[i];
 
     EXPECT_EQ(c.tags, (std::vector<size_t>{2, 3}));
     EXPECT_EQ(c.values, (std::vector<int>{-3, -4}));
 }
 
-TEST(Columnar, Simple)
-{
+TEST(Columnar, Simple) {
     auto tags = std::vector<std::string>{"ali", "john"};
 
-    struct Columnar
-    {
+    struct Columnar {
         DataFrame<std::string, std::string> favorite_color;
         DataFrame<std::string, int> num_toes;
         DataFrame<std::string, int> num_teeth;
     } df{
         {tags, {"green", "blue"}},
-        {tags, {6, 10}},
-        {tags, {18, 32}}};
+        {tags, {6, 10}          },
+        {tags, {18, 32}         }
+    };
 
-    auto toes_per_tooth = Join::collate(df.num_toes, df.num_teeth, [](int num_toes, int num_teeth)
-                                        { return float(num_toes) / num_teeth; });
+    auto toes_per_tooth = Join::collate(
+        df.num_toes, df.num_teeth, [](int num_toes, int num_teeth) { return float(num_toes) / num_teeth; });
     EXPECT_EQ(toes_per_tooth.size(), 2);
     EXPECT_EQ(toes_per_tooth.values[0], 6.f / 18);
     EXPECT_EQ(toes_per_tooth.values[1], 10.f / 32);
 }
 
-struct O1
-{
+struct O1 {
     std::string favorite_color;
     int num_toes;
 };
-struct O2
-{
+struct O2 {
     int num_teeth;
 };
 
-struct O3 : O1, O2
-{
+struct O3 : O1, O2 {
     O3(const O1 &o1, const O2 &o2) : O1(o1), O2(o2) {}
 };
 
-bool operator==(const O3 &left, const O3 &right)
-{
-    return (left.favorite_color == right.favorite_color) && (left.num_toes == right.num_toes) && (left.num_teeth == right.num_teeth);
+bool operator==(const O3 &left, const O3 &right) {
+    return (left.favorite_color == right.favorite_color) && (left.num_toes == right.num_toes) &&
+           (left.num_teeth == right.num_teeth);
 }
 
-std::ostream &operator<<(std::ostream &s, const O3 &o3)
-{
+std::ostream &operator<<(std::ostream &s, const O3 &o3) {
     s << "O3(" << o3.favorite_color << ", " << o3.num_toes << ", " << o3.num_teeth << ')';
     return s;
 }
 
-TEST(Join, Structs)
-{
+TEST(Join, Structs) {
     auto df1 = DataFrame<std::string, O1>{
-        {"ali", "john"},
-        {O1{.favorite_color = "green", .num_toes = 6}, O1{.favorite_color = "blue", .num_toes = 10}}};
+        {"ali",                                        "john"                                      },
+        {O1{.favorite_color = "green", .num_toes = 6}, O1{.favorite_color = "blue", .num_toes = 10}}
+    };
 
     auto df2 = DataFrame<std::string, O2>{
-        {"ali", "john"},
-        {O2{.num_teeth = 18}, O2{.num_teeth = 32}}};
+        {"ali",               "john"             },
+        {O2{.num_teeth = 18}, O2{.num_teeth = 32}}
+    };
 
-    auto g = Join::collate(df1, df2,
-                           [](const O1 &left, const O2 &right)
-                           { return O3(left, right); });
+    auto g = Join::collate(df1, df2, [](const O1 &left, const O2 &right) { return O3(left, right); });
 
     EXPECT_EQ(g.tags, df1.tags);
     EXPECT_EQ(g.values[0], O3(O1{"green", 6}, O2{18}));
     EXPECT_EQ(g.values[1], O3(O1{"blue", 10}, O2{32}));
 }
 
-TEST(Join, Strings)
-{
-    auto df1 = DataFrame<std::string, float>{{"ali", "john"}, {1., 2.}};
-    auto df2 = DataFrame<std::string, float>{{"ali", "john"}, {10., 20.}};
+TEST(Join, Strings) {
+    auto df1 = DataFrame<std::string, float>{
+        {"ali", "john"},
+        {1.,    2.    }
+    };
+    auto df2 = DataFrame<std::string, float>{
+        {"ali", "john"},
+        {10.,   20.   }
+    };
 
     auto g = Join::sum(df1, df2);
 
@@ -96,10 +99,15 @@ TEST(Join, Strings)
     EXPECT_EQ(g.values, std::vector<float>({11., 22.}));
 }
 
-TEST(Join, Simple)
-{
-    auto df1 = DataFrame<int, float>{.tags = {1, 2, 3}, .values = {10., 20., 30.}};
-    auto df2 = DataFrame<int, float>{.tags = {1, 2, 3}, .values = {-11., -22., -33.}};
+TEST(Join, Simple) {
+    auto df1 = DataFrame<int, float>{
+        .tags = {1,   2,   3  },
+          .values = {10., 20., 30.}
+    };
+    auto df2 = DataFrame<int, float>{
+        .tags = {1,    2,    3   },
+          .values = {-11., -22., -33.}
+    };
 
     auto g = Join::sum(df1, df2);
 
@@ -109,10 +117,15 @@ TEST(Join, Simple)
     EXPECT_EQ(g.values, (std::vector<float>{-1., -2., -3.}));
 }
 
-TEST(Join, DuplicatesLeft)
-{
-    auto df1 = DataFrame<int, float>{{1, 2, 2, 3}, {10., 20., 100., 30.}};
-    auto df2 = DataFrame<int, float>{{1, 2, 3}, {-11., -22., -33.}};
+TEST(Join, DuplicatesLeft) {
+    auto df1 = DataFrame<int, float>{
+        {1,   2,   2,    3  },
+        {10., 20., 100., 30.}
+    };
+    auto df2 = DataFrame<int, float>{
+        {1,    2,    3   },
+        {-11., -22., -33.}
+    };
 
     auto g = Join::sum(df1, df2);
 
@@ -121,10 +134,15 @@ TEST(Join, DuplicatesLeft)
     EXPECT_EQ(g.tags, (std::vector<int>{1, 2, 3}));
     EXPECT_EQ(g.values, (std::vector<float>{-1., 98., -3.}));
 }
-TEST(Join, Binary)
-{
-    auto df1 = DataFrame<int, float>{.tags = {0, 0, 1}, .values = {10., 20., 30.}};
-    auto df2 = DataFrame<int, float>{.tags = {0, 1, 1}, .values = {-11., -22., -33.}};
+TEST(Join, Binary) {
+    auto df1 = DataFrame<int, float>{
+        .tags = {0,   0,   1  },
+          .values = {10., 20., 30.}
+    };
+    auto df2 = DataFrame<int, float>{
+        .tags = {0,    1,    1   },
+          .values = {-11., -22., -33.}
+    };
 
     auto g = Join::sum(df1, df2);
 
@@ -133,14 +151,17 @@ TEST(Join, Binary)
     EXPECT_EQ(g.values, (std::vector<float>{19., 8., 8.}));
 }
 
-TEST(Join, SimplePair)
-{
-    auto df1 = DataFrame<int, float>{{1, 2, 3}, {10., 20., 30.}};
-    auto df2 = DataFrame<int, float>{{1, 2, 3}, {-11., -22., -33.}};
+TEST(Join, SimplePair) {
+    auto df1 = DataFrame<int, float>{
+        {1,   2,   3  },
+        {10., 20., 30.}
+    };
+    auto df2 = DataFrame<int, float>{
+        {1,    2,    3   },
+        {-11., -22., -33.}
+    };
 
-    auto g = Join::collate(df1, df2,
-                           [](float v1, float v2)
-                           { return std::pair(v1, v2); });
+    auto g = Join::collate(df1, df2, [](float v1, float v2) { return std::pair(v1, v2); });
 
     EXPECT_EQ(g.size(), 3);
     EXPECT_EQ(g.tags, (std::vector<int>{1, 2, 3}));
@@ -149,9 +170,11 @@ TEST(Join, SimplePair)
     EXPECT_EQ(g.values[2], std::pair(30.f, -33.f));
 }
 
-TEST(Utilities, first_tags)
-{
-    auto df = DataFrame<int, float>{{1, 2, 2, 3}, {10., 20., 100., 30.}};
+TEST(Utilities, first_tags) {
+    auto df = DataFrame<int, float>{
+        {1,   2,   2,    3  },
+        {10., 20., 100., 30.}
+    };
     auto dft = uniquify_tags(df);
 
     EXPECT_TRUE((std::is_same<decltype(dft.values[100]), NoValue>::value));
@@ -160,9 +183,11 @@ TEST(Utilities, first_tags)
     EXPECT_EQ(dft.tags, (std::vector<int>{1, 2, 3}));
 }
 
-TEST(Group, sum)
-{
-    auto df = DataFrame<int, float>{{1, 2, 2, 3}, {10., 20., 100., 30.}};
+TEST(Group, sum) {
+    auto df = DataFrame<int, float>{
+        {1,   2,   2,    3  },
+        {10., 20., 100., 30.}
+    };
 
     auto g = Group::sum(df);
 
@@ -170,11 +195,16 @@ TEST(Group, sum)
     EXPECT_EQ(g.values, (std::vector<float>{10., 120., 30.}));
 }
 
-TEST(Index, Simple)
-{
-    auto df = DataFrame<int, float>{.tags = {1, 2, 3, 4}, .values = {10., 20., 30., 40.}};
+TEST(Index, Simple) {
+    auto df = DataFrame<int, float>{
+        .tags = {1,   2,   3,   4  },
+          .values = {10., 20., 30., 40.}
+    };
 
-    auto i = DataFrame<int, float>{.tags = {2, 3}, .values = {-20., -30.}};
+    auto i = DataFrame<int, float>{
+        .tags = {2,    3   },
+          .values = {-20., -30.}
+    };
 
     auto g = df[i];
 
@@ -184,11 +214,15 @@ TEST(Index, Simple)
     EXPECT_EQ(g.values, (std::vector<float>{20., 30.}));
 }
 
-TEST(Index, NoValues)
-{
-    auto df = DataFrame<int, float>{.tags = {1, 2, 3, 4}, .values = {10., 20., 30., 40.}};
+TEST(Index, NoValues) {
+    auto df = DataFrame<int, float>{
+        .tags = {1,   2,   3,   4  },
+          .values = {10., 20., 30., 40.}
+    };
 
-    auto i = DataFrame<int, NoValue>{.tags = {2, 3}};
+    auto i = DataFrame<int, NoValue>{
+        .tags = {2, 3}
+    };
 
     auto g = df[i];
 
